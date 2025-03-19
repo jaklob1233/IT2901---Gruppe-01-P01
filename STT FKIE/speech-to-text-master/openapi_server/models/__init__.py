6f1dafd3b43097a5d8f3118557f8bc811a097b29
EPA1 <32443679+EPA1@users.noreply.github.com>
2025-03-19 09:36:54 +0100

    Add files via upload
    added openapi models to stt

---
+++ STT FKIE/speech-to-text-master/openapi_server/models/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+# import models into model package
+from openapi_server.models.error import Error
+from openapi_server.models.on_new_text_post_request import OnNewTextPostRequest
+from openapi_server.models.provide_audio_request import ProvideAudioRequest
