272415a28f096b4efd16391d6db8000388145e71
Gustav Hovde <gustav.s.hovde@ntnu.no>
2025-04-01 11:25:39 +0200

    Changed image tag

--- STT FKIE/speech-to-text-master/build_and_run.sh
+++ STT FKIE/speech-to-text-master/build_and_run.sh
@@ -2,7 +2,7 @@ IMAGE_NAME="speechtotext"
 VERSION="latest"
 
 # build Docker
-docker build . -t container-registry.gitlab.cc-asp.fraunhofer.de/ist-201/images/$IMAGE_NAME:$VERSION
+docker build . -t $IMAGE_NAME:$VERSION
 
 # run Docker image
-docker run --rm -d --network host container-registry.gitlab.cc-asp.fraunhofer.de/ist-201/images/$IMAGE_NAME:$VERSION
+docker run --rm -d --network host $IMAGE_NAME:$VERSION
