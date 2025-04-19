13cca7132860e593e9feca7169d940025fe9e7ba
Gustav Hovde <gustav.s.hovde@ntnu.no>
2025-04-19 12:36:31 +0200

    Added changes to test file, added __init__.py, and small changes to requirements

--- STT FKIE/speech-to-text-master/test/test_speechtotext.py
+++ STT FKIE/speech-to-text-master/test/test_speechtotext.py
@@ -1,4 +1,5 @@
 import time
+import os
 import base64
 import threading
 import sounddevice as sd  # Use sounddevice for WSLg compatibility
@@ -14,9 +15,12 @@ from pydub import AudioSegment
 import os
 
 os.makedirs("logs", exist_ok=True)
+from pydub import AudioSegment 
 
 # Configure the log file and logger
 filename = "logs/server.log"
+log_dir = os.path.dirname(filename)
+os.makedirs(log_dir, exist_ok=True)
 
 logging.basicConfig(
     level=logging.DEBUG,
