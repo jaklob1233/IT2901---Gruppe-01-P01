05b4721074e45f6997a453fac9153d4af6066f04
Gustav Hovde <gustav.s.hovde@ntnu.no>
2025-04-22 09:26:11 +0200

    Changed downloadModels.py

--- STT FKIE/speech-emotion-recognition/downloadModels.py
+++ STT FKIE/speech-emotion-recognition/downloadModels.py
@@ -4,23 +4,34 @@ from funasr import AutoModel
 from funasr.utils.postprocess_utils import rich_transcription_postprocess
 from huggingface_hub import snapshot_download
 
-script_dir = Path(__file__).resolve().parent
-superb_dir = script_dir / "Superb"
+def downloadSuperb():
+    """
+    Download the SUPERB model from Hugging Face Hub.
+    """
+    # Define the directory where the model will be downloaded
+    script_dir = Path(__file__).resolve().parent
+    superb_dir = script_dir / "Superb"
 
-# This will download the entire repository to the specified directory
-#snapshot_download(repo_id="superb/wav2vec2-large-superb-er", local_dir=str(superb_dir), repo_type="model")
+    # Download the model from Hugging Face Hub
+    snapshot_download(repo_id="superb/wav2vec2-large-superb-er", local_dir=str(superb_dir), repo_type="model")
 
-# Set the cache directory where the model will be downloaded and stored
-os.environ["MODELSCOPE_CACHE"] = os.path.join(os.path.dirname(__file__), "Sensevoice", "model")
+def downloadSenseVoice():
+    """
+    Download the SenseVoice model.
+    """   
+    # Set the cache directory where the model will be downloaded and saved
+    os.environ["MODELSCOPE_CACHE"] = os.path.join(os.path.dirname(__file__), "Sensevoice", "model")
 
-# Define model to download
-model_name = "iic/SenseVoiceSmall"
+    # Define model to download
+    model_name = "iic/SenseVoiceSmall"
 
-# Download and save the model
-model = AutoModel(
-    model=model_name,
-    vad_model="iic/speech_fsmn_vad_zh-cn-16k-common-pytorch",
-    vad_kwargs={"max_single_segment_time": 30000},
-    trust_remote_code=True
-)
+    # Download and save the model
+    model = AutoModel(
+        model=model_name,
+        vad_model="iic/speech_fsmn_vad_zh-cn-16k-common-pytorch",
+        vad_kwargs={"max_single_segment_time": 30000},
+        trust_remote_code=True
+    )
 
+downloadSuperb()
+downloadSenseVoice()
