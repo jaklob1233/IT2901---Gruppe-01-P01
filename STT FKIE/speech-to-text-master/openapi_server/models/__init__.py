3d1ee94ce9d41e14300533f3c790e1de46ec5cd2
Gustav Hovde <gustav.s.hovde@ntnu.no>
2025-05-07 09:31:29 +0200

    Change name for STT FKIE to STTE

--- STT FKIE/speech-to-text-master/openapi_server/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# flake8: noqa
-# import models into model package
-from openapi_server.models.error import Error
-from openapi_server.models.on_new_text_post_request import OnNewTextPostRequest
-from openapi_server.models.provide_audio_request import ProvideAudioRequest
